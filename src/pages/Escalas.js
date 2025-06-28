faaea384d938ee89a3551b1938021b76c33b0d80
Wiliam Rizzo <wiliamrizzo@gmail.com>
2025-06-27 22:33:19 -0300

    header tela escalas

--- src/pages/Escalas.js
+++ src/pages/Escalas.js
@@ -17,6 +17,8 @@ export default function Escalas() {
           <ArrowLeft size={24} strokeWidth={1.5} />
         </button>
         <h2 className="escalas-title">Escalas</h2>
+        {/* Placeholder vazio que ocupa o espaço do botão */}
+        <div className="placeholder" />
       </header>
     </div>
   );
